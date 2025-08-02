640773eaa4da5a65ab4b2e9bccc496ccea36455b
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-08-03 00:06:42 +0700

    styling and CORS access whitelist

--- api/total.js
+++ api/total.js
@@ -2,25 +2,34 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = (req, res) => {
-  const filePath = path.join(__dirname, "..", "dataTotal.json");
+    //CORS Handle
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: "Gagal fetch." });
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
     }
 
-    const parsedData = JSON.parse(data);
+    const filePath = path.join(__dirname, "..", "dataTotal.json");
 
-    res.status(200).json({
-      totalDeposit: {
-        label: "Total Deposit",
-        amount: `IDR ${Number(parsedData.totalDeposit.amount).toLocaleString("id-ID")}`,
-        count: `${parsedData.totalDeposit.count} Deposit(s)`
-      },
-      totalRegistration: {
-        label: "Total Registration",
-        count: `${parsedData.totalRegistration.count} Registration`
-      }
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: "Gagal fetch." });
+        }
+
+        const parsedData = JSON.parse(data);
+
+        res.status(200).json({
+            totalDeposit: {
+                label: "Total Deposit",
+                amount: `IDR ${Number(parsedData.totalDeposit.amount).toLocaleString("id-ID")}`,
+                count: `${parsedData.totalDeposit.count} Deposit(s)`
+            },
+            totalRegistration: {
+                label: "Total Registration",
+                count: `${parsedData.totalRegistration.count} Registration`
+            }
+        });
     });
-  });
 };
